fab5d681d76f03a469941546259f30c94b7e2152
whitia <whitia7@gmail.com>
2020-03-22 21:30:05 +0900

    modify layout

--- components/About.vue
+++ components/About.vue
@@ -3,19 +3,13 @@
     <div class="content-wrap">
       <div class="content">
         <h2>ABOUT</h2>
-        <p>
-          アーティスト名「SIGERO」に対して、楽曲提供/ディレクション時における名義は「吉田シゲロウ」となっている。が、明確な区別があるわけではない。おおむね本名だが、「SIGERO」の「シ」には「H」が付かない。
-          兵庫県芦屋に生まれ、西宮市甲子園に育つ。誕生日は10月15日、年齢の公称は少しはにかみます。
-========== ●血液型のひみつ● ==========
-
-日本人では最も比率が高いとされるA型だが、周囲の人間はAB型がやたらと多いのでA型が多いなんてとても信じられないとぼやくA型。几帳面な面倒臭がり、神経質なズボラ？
-
-========== ●好みのひみつ● ==========
-
-音楽／曰く「せつないフェチ」であり、甘く憂いを含んだサウンドには目がない。雑食で守備範囲は広いものの黒い系はあまり触手が伸びない傾向にあった。が、昨今はソウル好きな自分に気がつき、愉しみが拡がったと喜んでいる。
-
-Tahiti80、PrimalScream、BECK、ELO、XTC、Squeeze、MyBloodyValentine、David Bowie、SoulWax、 TheStoneRoses、TheMusic、SigueSigueSputnik、ChemicalBrothers、Radiohead、DefSchool、
-        </p>
+        <div class="line"></div>
+        <h3>吉田シゲロウ</h3>
+        <p>兵庫県芦屋市出身の作曲家/サウンドディレクター/シンガー。</p>
+        <p>ProToolsを中心としたプライベートスタジオ「Honeycomb-Star」開設し、サントラやリミックス、ゲームSE制作などを数多く手掛ける。</p>
+        <p>M.H.R.Accentsよりソロ・プロジェクト名義「SIGERO」で1stミニ・アルバムをリリース。</p>
+        <p>音楽制作の傍ら、雑誌の編集・執筆も手掛けるエディター/ライターとしての側面も持つ。</p>
+        <nuxt-link to="/" class="button">より詳しいプロフィールへ</nuxt-link>
       </div>
     </div>
   </div>
