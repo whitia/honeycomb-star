6219831bb48949e0c7ce9166c441730de503a73f
whitia <whitia7@gmail.com>
2020-03-23 10:16:31 +0900

    add contact form

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <div class="footer">
-      <p>
+      <div class="mb-2">
         <span><nuxt-link to="/">HOME</nuxt-link></span>
         <span><nuxt-link to="/about">ABOUT</nuxt-link></span>
         <span><nuxt-link to="/works">WORKS</nuxt-link></span>
         <span><nuxt-link to="/">BLOG</nuxt-link></span>
         <span><nuxt-link to="/">SHOP</nuxt-link></span>
         <span><nuxt-link to="/contact">CONTACT</nuxt-link></span>
-      </p>
-      <p>Copyright &copy; 2020 SIGERO YOSHIDA All Rights Reserved.</p>
+      </div>
+      <div>Copyright &copy; 2020 SIGERO YOSHIDA All Rights Reserved.</div>
     </div>
   </div>
 </template>
--- components/home/Contact.vue
+++ components/home/Contact.vue
@@ -4,7 +4,28 @@
       <div class="content">
         <h2>CONTACT</h2>
         <div class="line"></div>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScjPB2zZgQCPEvfnBMsXoAj06Smx9h_dvUu26LFTy2Wbg4gwQ/viewform?embedded=true" width="640" height="709" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+
+<form name="contact" method="POST" netlify>
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
       </div>
     </div>
   </div>
